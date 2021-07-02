d438cf31c6b07f19d1b07a0c22a3ff1b0c590ac6
Valeriya <labramka2402@gmail.com>
2021-07-02 09:55:28 +0300

    5

--- Practice/Practice.cpp
+++ Practice/Practice.cpp
@@ -21,6 +21,7 @@ char split_for_depth(char& line);
 char split_line(char& line);
 const char delimetr = ';';
 const char* filename = "D://data2.txt";
+char split_for_depth2(char& line);
 
 FILE* fp;
 
@@ -31,7 +32,7 @@ void Display()
     printf("|                           Меню                           | \n");
     printf("|              Введите номер опции:                        | \n");
     printf("|             1. Вывод списка                              | \n");
-    printf("|              2. Вывод суммарной длины рек,               | \n|         глубина которых меньше 50 метров                 | \n");
+    printf("|             2. Вывод суммарной длины рек,                | \n|         глубина которых меньше 50 метров                 | \n");
     printf("|             3. Очистить                                  | \n");
     printf("+----------------------------------------------------------+\n");
 
@@ -97,9 +98,9 @@ char println()
         }
     }
     fclose(fp);
-    printf("************************************************************\n");
+    printf("+----------------------------------------------------------+\n");
     printf("|   Название реки   |   Длина реки     |   Средняя глубина |\n");
-    printf("************************************************************\n");
+    printf("+----------------------------------------------------------+\n");
     for (int i = 0; i < k; ++i)
     {
         split_line(*line[i]);
@@ -108,11 +109,99 @@ char println()
 }
 char split_line(char& line)
 {
+    char* lines[3];
+    int i = 0;
+    char* piece = strtok(&line, ";");
+    while (piece != NULL)
+    {
 
+        lines[i] = piece;
+        piece = strtok(NULL, ";");
+        i++;
+    }
+    for (int k = 0; k < i; ++k)
+    {
+        printf("   %13s", lines[k]);
+    }
+    return 0;
 }
 char search()
 {
-    const char* filename = "D://data2.txt";
+    char line[M2][M1];
+    int k = 0;
+    char* lines[3];
+    int i = 0;
+    fp = fopen(filename, "r");
+    // чтение из файла
+    if (fp == NULL)
+    {
+        perror("Ошибка");
+        return 1;
+    }
+    else
+    {
+        while (fgets(line[k], M1, fp) != NULL)
+        {
+            k++;
+        }
+    }
+    fclose(fp);
+    for (int i = 0; i < k; ++i)
+    {
+        split_for_depth2(*line[i]);
+    }
+    return 0;
+}
+char split_for_depth(char& line)
+{
+    char* lines[3];
+    char a;
+    int i = 0, k = 0, max = 3;
+    int var;
+    char* depth[M1];
+    char* piece = strtok(&line, ";");
+    while (piece != NULL)
+    {
+
+        lines[i] = piece;
+        piece = strtok(NULL, ";");
+        i++;
+        if (i == 2)
+        {
+            depth[k] = piece;
+            k++;
+        }
+    }
+    for (int j = 0; j < k; j++)
+    {
+        //if((int)depth[j] < 50)
+        printf("%d\n", (depth[j] - '0'));
+    }
     return 0;
 }
 
+char split_for_depth2(char& line)
+{
+    char* lines[3];
+    char a;
+    int i = 0, k = 0, max = 3;
+    int var;
+    char* depth[M1];
+    char* piece = strtok(&line, ";");
+    while (piece != NULL)
+    {
+
+        lines[i] = piece;
+        piece = strtok(NULL, ";");
+        i++;
+        for (int j = 0; j < k; j++)
+        {
+            //if((int)depth[j] < 50)
+            printf("%d\n", (depth[j] - '0'));
+        }
+
+
+
+        return 0;
+    }
+}
